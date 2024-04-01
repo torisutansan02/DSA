71c8048c2e47092ea1a12fb6f934a546e28b50e6
Tristan C <torisutansan02@gmail.com>
2024-03-31 17:42:09 -0700

    updating readme and finishing linked linked_list implementation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# dsa
+# DSA
+
+The purpose of this repository is to educate myself on data structures and algorithms. I am going to use Python as the programming language for all of my files in this repository.
--- linked_lists/linked_list.py
+++ linked_lists/linked_list.py
@@ -53,6 +53,7 @@ class LinkedList:
         """
         Searches for the first node containing data that matches the key.
         Returns the node or 'None' if not found.
+
         Takes O(n) time.
         """
 
@@ -64,6 +65,58 @@ class LinkedList:
             else:
                 current = current.next_node
         return None
+    
+    def insert(self, data, index):
+        """
+        Inserts a new Node containing data at index position.
+        Insertion takes O(1) time but finding the node at the
+        insertion point takes O(n) time.
+
+        Takes overall O(n) time.
+        """
+
+        if index == 0:
+            self.add(data)
+
+        if index > 0:
+            new = Node(data)
+
+            position = index
+            current = self.head
+
+            while position > 1:
+                current = current.next_node
+                position -= 1
+
+            prev_node = current
+            next_node = current.next_node
+
+            prev_node.next_node = new
+            new.next_node = next_node
+
+    def remove(self, key):
+        """
+        Removes Node containing data that matches the key.
+        Returns the node or None if key doesn't exist.
+        Takes O(n) time.
+        """
+        
+        current = self.head
+        previous = None
+        found = False
+
+        while current and not found:
+            if current.data == key and current is self.head:
+                found = True
+                self.head = current.next_node
+            elif current.data == key:
+                found = True
+                previous.next_node = current.next_node
+            else:
+                previous = current
+                current = current.next_node
+
+        return current
 
     def __repr__(self):
         """
